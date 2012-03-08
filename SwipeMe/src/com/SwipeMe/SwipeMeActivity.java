7e30aa9e16d5c8fbc58392bd283274c248b3f430
Sherif Makhlouf <sherifmakhlouf91@gmail.com>
2012-03-08 20:44:32 +0200

    Adding Swipe Action to a button
    Signed-off-by: Sherif Makhlouf <sherifmakhlouf91@gmail.com>

--- SwipeMe/src/com/SwipeMe/SwipeMeActivity.java
+++ SwipeMe/src/com/SwipeMe/SwipeMeActivity.java
@@ -2,6 +2,11 @@ package com.SwipeMe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+// importing the onSwipeListener
+import SwipeMe.OnSwipeListener;
 
 public class SwipeMeActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +14,34 @@ public class SwipeMeActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button test = (Button) findViewById(R.id.button1);
+        
+        //adding onTouch action to test button
+        //but by sending it the on swipe listener
+        test.setOnTouchListener(new OnSwipeListener() {
+			
+			@Override
+			public void onSwipe(View v, MotionEvent event) {
+				// TODO Auto-generated method stub
+				
+				//checking the direction of the swipe
+				switch(getSwipeTo())
+				{
+				case SWIPED_LEFT :
+					//swiped to left
+					break;
+				case SWIPED_RIGHT :
+					//swiped to right
+					break;
+				case SWIPED_UP :
+					//swiped up
+					break;
+				case SWIPED_DOWN :
+					//swiped down
+					break;
+				}
+			}
+		});
     }
 }
